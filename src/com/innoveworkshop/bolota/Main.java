2015ffb4a51b315a0dc78b2c2545709d8cf27a10
Nathan Campos <nathan@innoveworkshop.com>
2025-01-22 12:06:49 +0000

    Added an IntelliJ project
    Created a simple IntelliJ project and added the application's entry
    point class and an Ant build script.

---
+++ src/com/innoveworkshop/bolota/Main.java
@@ -0,0 +1,17 @@
+package com.innoveworkshop.bolota;
+
+/**
+ * Program's main runnable class.
+ */
+public class Main {
+
+	/**
+	 * Application's main entry point.
+	 *
+	 * @param args Command line arguments.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Bolota!");
+	}
+
+}
